b451a08fce1ee663731d86b4fd94ca81c9660fee
shahrukh-ak <55116845+shahrukh-ak@users.noreply.github.com>
2026-04-13 13:17:36 -0400

    Update Analysis of Beijing Multi Site Air Quality Data_professional.R

--- Analysis of Beijing Multi Site Air Quality Data_professional.R
+++ Analysis of Beijing Multi Site Air Quality Data_professional.R
@@ -1,9 +1,4 @@
 # Beijing Multi-Site Air Quality Analysis
-#
-# Professionalized version of the original project file.
-# The code and stated results are retained from the source as closely as possible.
-# Output text from the original file has been converted to comments where needed for readability.
-#
 
 # Analysis-of-Beijing-Multi-Site-Air-Quality-Data
 # The data set Beijing Multi-Site Air-Quality Data provided in the UCI Machine Learning Repository includes hourly air pollutants data from 12 nationally-controlled air-quality monitoring sites. The air-quality data are from the Beijing Municipal Environmental Monitoring Center. The meteorological data in each air-quality site are matched with the nearest weather station from the China Meteorological Administration. The time period is from March 1st, 2013 to February 28th, 2017. Missing data are denoted as NA. Source: Zhang, S., Guo, B., Dong, A., He, J., Xu, Z. and Chen, S.X. (2017) Cautionary Tales on Air-Quality Improvement in Beijing. Proceedings of the Royal Society A, Volume 473, No. 2205, Pages 04-57.<br>
